6a6b99d32e1a9b443d93c0c950f57d42782cec18
Dmitry Bekker <gusyaraaa@yandex.ru>
2021-07-29 20:13:18 +0300

    app

---
+++ routes/add.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const router = Router();
+const Course = require("../models/course");
+
+router.get("/", (req, res) => {
+  res.render("add", {
+    title: "Добавить курс",
+    isAdd: true,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.image);
+
+  await course.save();
+
+  res.redirect("/courses");
+});
+
+module.exports = router;
